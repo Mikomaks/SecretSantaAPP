d912b52b35436b73d582edf67ebb82d345e05c8c
Mikomaks <ytmikos@gmail.com>
2025-11-28 12:28:06 +0100

    Initial commit

---
+++ secretsantafront/lib/models/user.dart
@@ -0,0 +1,21 @@
+class User{
+  int id;
+  String name;
+  String email;
+
+  User(this.id, this.name, this.email);
+
+  User.fromJson(Map<String, dynamic> json)
+      : id = json['id'],
+        name = json['name'],
+        email = json['email'];
+
+  Map<String, dynamic> toJson()
+  {
+    return {
+      'id': id,
+      'name': name,
+      'email': email,
+    };
+  }
+}
